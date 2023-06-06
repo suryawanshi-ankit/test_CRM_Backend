7ef61092349376f2f3776a31a9ffe503ba16ac62
suryawanshi-ankit <suryawanshiankit97@gmail.com>
2023-06-06 13:06:59 +0530

    setup for https server

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const express = require('express');
+const router = require('./routes');
+const https = require('https');
+const fs = require('fs');
+const PORT = 8080
+
 require('./dbConn');
+
 const app = express();
 
-app.listen(8080, () => console.log("listning on port 8080"));
+app.use(express.json());
+app.use('/api.recruitcrm.io/v1', router);
+
+const options = {
+  key: fs.readFileSync('/home/developer/private-key.pem'),
+  cert: fs.readFileSync('/home/developer/certificate.pem')
+};
+
+
+const server = https.createServer(options, app);
+
+server.listen(PORT, () => console.log("listning on port 8080"));
